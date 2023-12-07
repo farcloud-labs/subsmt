194c5b34b2ea6f5a03acb1d8b8d026bdf21c047a
YanOctavian <iu4ryhiuenb@proton.me>
2023-12-07 23:29:33 +0800

    update

--- SMT-api/src/lib.rs
+++ SMT-api/src/lib.rs
@@ -1,17 +1,17 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#[cfg(test)]
-mod tests;
-use parity_scale_codec::alloc::vec::Vec;
-
-#[cfg(not(feature="derive"))]
-use parity_scale_codec_derive::{Encode, Decode};
-
-// use parity_scale_codec::{Encode, Decode};
-
-#[derive(Debug, PartialEq, Encode, Decode)]
-pub struct Student {
-    name: Vec<u8>,
-    score: u8,
-    id: u16,
-}
+// #[cfg(test)]
+// mod tests;
+// use parity_scale_codec::alloc::vec::Vec;
+//
+// // #[cfg(not(feature="derive"))]
+// // use parity_scale_codec_derive::{Encode, Decode};
+// //
+// // // use parity_scale_codec::{Encode, Decode};
+// //
+// // #[derive(Debug, PartialEq, Encode, Decode)]
+// // pub struct Student {
+// //     name: Vec<u8>,
+// //     score: u8,
+// //     id: u16,
+// // }
 
--- SMT-api/src/tests.rs
+++ SMT-api/src/tests.rs
@@ -1,28 +1,28 @@
-use hex::encode;
-use super::*;
-use parity_scale_codec::Encode;
-use parity_scale_codec::alloc::vec::Vec;
-
-#[test]
-fn main() {
-    let a = Student {
-        name: vec![0; 4],
-        score: 64,
-        id: 42,
-    };
-
-    let a_e = a.encode();
-    // fixme 为什么是16在前
-    println!("{:?}", a_e);
-    let h_s = hex::encode(a_e);
-    println!("{:?}", h_s);
-    let a_n_e = a.name.encode();
-    println!("{:?}", a_n_e);
-    let a_n_s = a.score.encode();
-    println!("{:?}", a_n_s);
-    let a_n_id = a.id.encode();
-    println!("{:?}", a_n_id);
-    let h = hex::encode(a_n_id);
-    println!("{:?}", h);
-
-}
+// use hex::encode;
+// use super::*;
+// use parity_scale_codec::Encode;
+// use parity_scale_codec::alloc::vec::Vec;
+//
+// #[test]
+// fn main() {
+//     let a = Student {
+//         name: vec![0; 4],
+//         score: 64,
+//         id: 42,
+//     };
+//
+//     let a_e = a.encode();
+//     // fixme 为什么是16在前
+//     println!("{:?}", a_e);
+//     let h_s = hex::encode(a_e);
+//     println!("{:?}", h_s);
+//     let a_n_e = a.name.encode();
+//     println!("{:?}", a_n_e);
+//     let a_n_s = a.score.encode();
+//     println!("{:?}", a_n_s);
+//     let a_n_id = a.id.encode();
+//     println!("{:?}", a_n_id);
+//     let h = hex::encode(a_n_id);
+//     println!("{:?}", h);
+//
+// }
