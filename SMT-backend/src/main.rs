f3ff5d5a21237b8e9b1ac8e70a8e77903f3e33ba
YanOctavian <iu4ryhiuenb@proton.me>
2024-01-14 14:51:41 +0800

    update

--- SMT-backend/src/main.rs
+++ SMT-backend/src/main.rs
@@ -1,4 +1,13 @@
-
+use rocksdb::{OptimisticTransactionDB, SingleThreaded, WriteBatchWithTransaction, WriteOptions};
 fn main() {
     // println!("Hello, world!");
+    let db: OptimisticTransactionDB<SingleThreaded> = OptimisticTransactionDB::open_default("db/rocksdb").unwrap();
+    let mut txn = db.transaction();
+    let mut snap = db.snapshot();
+    db.get(b"key1").unwrap();
+    db.delete(b"key1").unwrap();
+    db.flush().unwrap();
+    // db.write(batch)
+    txn.put(b"key2", b"value2").unwrap();
+    txn.commit().unwrap();
 }
