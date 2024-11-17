0d3fca14d52b327310ab92905d18de9d70a5afd7
YanOctavian <iu4ryhiuenb@proton.me>
2024-11-17 23:19:11 +0800

    update

--- ink-contract/SMT/lib.rs
+++ ink-contract/SMT/lib.rs
@@ -1,44 +1,53 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+
+// 如何表达事件？？？
 #[ink::contract]
 mod smt {
+    use smt_primitives::kv::{SMTKey, SMTValue};
+    use smt_primitives::verify::Proof;
+    use smt_primitives::verify::verify as smt_verify;
+    use smt_primitives::keccak_hasher::Keccak256Hasher;
+
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
-    pub struct Smt {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
-    }
+    pub struct Smt;
 
+    impl Default for Smt {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+    
     impl Smt {
+        
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self::default()
         }
 
+    
         /// Constructor that initializes the `bool` value to `false`.
         ///
         /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            Self::new()
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn smt_verify(&self, proof: Proof<SMTKey, SMTValue>) -> bool {
+            smt_verify::<Keccak256Hasher>(
+                proof.path,
+                proof.value_hash,
+                proof.leave_bitmap,
+                proof.siblings,
+                proof.root
+            )
         }
     }
 
