58a46f2036ec218e5e8baaa212f02c364c742606
YanOctavian <iu4ryhiuenb@proton.me>
2024-11-17 14:57:16 +0800

    update

--- pallet/SMT/src/lib.rs
+++ pallet/SMT/src/lib.rs
@@ -20,12 +20,13 @@ mod benchmarking;
 pub mod pallet {
     use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
     use frame_system::pallet_prelude::*;
+    use primitives::verify::{self, Proof};
+    use scale_info::prelude::fmt::Debug;
+    use crate::weights::WeightInfo;
     use primitives::sparse_merkle_tree::{
         traits::{Hasher, Value},
         H256,
     };
-    use primitives::verify::{self, Proof};
-    use scale_info::prelude::fmt::Debug;
 
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
@@ -40,29 +41,18 @@ pub mod pallet {
         type SMTValue: Value + Default + Debug + Clone + TypeInfo + Encode + Decode + PartialEq;
 
         type SMTHasher: Hasher + Default;
-        // type BeforeVerify:
 
-        // type AfterVerify:
     }
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
-
-    // The pallet's runtime storage items.
-    // https://docs.substrate.io/v3/runtime/storage
-    #[pallet::storage]
-    // Learn more about declaring storage items:
-    // https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-    pub type Something<T> = StorageValue<_, u32>;
 
     // Pallets use events to inform users when important changes are made.
     // https://docs.substrate.io/v3/runtime/events-and-errors
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        /// Event documentation should end with an array that provides descriptive names for event
-        /// parameters. [something, who]
-        SomethingStored(u32, T::AccountId),
+
         SMTVerify {
             account: T::AccountId,
             path: H256,
@@ -73,10 +63,6 @@ pub mod pallet {
     // Errors inform users that something went wrong.
     #[pallet::error]
     pub enum Error<T> {
-        /// Error names should be descriptive.
-        NoneValue,
-        /// Errors should have helpful documentation associated with them.
-        StorageOverflow,
         SMTVerifyFaild,
     }
 
@@ -88,8 +74,10 @@ pub mod pallet {
     // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
     #[pallet::call]
     impl<T: Config> Pallet<T> {
+
+
         #[pallet::call_index(2)]
-        #[pallet::weight(Weight::from_parts(10_000, 0) + T::DbWeight::get().writes(1))]
+        #[pallet::weight(T::WeightInfo::smt_verify().saturating_mul(proof.siblings.len() as u64))]
         pub fn smt_verify(
             origin: OriginFor<T>,
             proof: verify::Proof<T::SMTKey, T::SMTValue>,
@@ -97,6 +85,7 @@ pub mod pallet {
             let who = ensure_signed(origin)?;
             Self::do_verify(who, proof)
         }
+
     }
 
     impl<T: Config> Pallet<T> {
