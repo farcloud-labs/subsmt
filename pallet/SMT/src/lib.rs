f5f2ee9e60a0ce9b3d76021661d519173bb72cf1
YanOctavian <iu4ryhiuenb@proton.me>
2024-11-17 16:52:54 +0800

    update

--- pallet/SMT/src/lib.rs
+++ pallet/SMT/src/lib.rs
@@ -44,6 +44,7 @@ pub mod pallet {
     }
 
     #[pallet::pallet]
+    #[pallet::without_storage_info]
     pub struct Pallet<T>(_);
 
     // Pallets use events to inform users when important changes are made.
