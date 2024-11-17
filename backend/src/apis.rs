ce6ded02928d86f296cc8abdd30dda4c222ed2f7
YanOctavian <iu4ryhiuenb@proton.me>
2024-11-17 21:18:17 +0800

    update

--- backend/src/apis.rs
+++ backend/src/apis.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
+#![allow(elided_named_lifetimes)]
 use crate::store::SMTStore;
 use ethers::core::k256::sha2::digest::Key;
 use kvdb_rocksdb::Database;
