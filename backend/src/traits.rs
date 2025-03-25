c7dea1ab5cdcfcac887b7ecec28a565263cc857c
YanOctavian <iu4ryhiuenb@proton.me>
2025-03-25 11:02:57 +0800

    update traits

--- backend/src/traits.rs
+++ backend/src/traits.rs
@@ -11,8 +11,9 @@ use utoipa::{ToSchema, __dev::ComposeSchema};
 
 /// Multi Sparse Merkle Tree Store trait
 /// Defines common operations for managing multiple Sparse Merkle Trees in a single store
-pub trait MSS<K, V, H>: Sized
+pub trait MSS<T, K, V, H>: Sized
 where
+    T: AsRef<[u8]>, // tree_id 类型参数，只要求能转换为字节切片
     K: Value + Clone + Serialize + ToSchema + Deserialize<'static> + ComposeSchema + Debug + TypeInfo,
     V: Default
         + Value
@@ -35,31 +36,31 @@ where
     fn open<P: AsRef<Path>>(path: P) -> std::io::Result<Self>;
 
     /// Create or open a new tree
-    fn new_tree_with_store<'a>(&'a self, tree_id: impl AsRef<[u8]> + 'a) -> Result<Self::Tree, Error>;
+    fn new_tree_with_store<'a>(&'a self, tree_id: T) -> Result<Self::Tree, Error>;
 
     /// Insert a value into a specific Merkle tree
-    fn update(&self, tree_id: impl AsRef<[u8]>, key: K, value: V) -> SMTResult<H256>;
+    fn update(&self, tree_id: T, key: K, value: V) -> SMTResult<H256>;
 
     /// Insert multiple values into a Merkle tree at once
-    fn update_all(&self, tree_id: impl AsRef<[u8]>, kvs: Vec<(K, V)>) -> SMTResult<H256>;
+    fn update_all(&self, tree_id: T, kvs: Vec<(K, V)>) -> SMTResult<H256>;
 
     /// Get the root hash of a specific tree
-    fn get_root(&self, tree_id: impl AsRef<[u8]>) -> Result<H256, Error>;
+    fn get_root(&self, tree_id: T) -> Result<H256, Error>;
 
     /// Get the value of a specific key in a particular tree
-    fn get_value(&self, tree_id: impl AsRef<[u8]>, key: K) -> Result<V, Error>;
+    fn get_value(&self, tree_id: T, key: K) -> Result<V, Error>;
 
     /// Get the Merkle proof for a specific key
-    fn get_merkle_proof(&self, tree_id: impl AsRef<[u8]>, key: K) -> Result<Proof<K, V>, Error>;
+    fn get_merkle_proof(&self, tree_id: T, key: K) -> Result<Proof<K, V>, Error>;
 
     /// Get the Merkle proof in raw bytes format
-    fn get_merkle_proof_old(&self, tree_id: impl AsRef<[u8]>, keys: Vec<K>) -> SMTResult<Vec<u8>>;
+    fn get_merkle_proof_old(&self, tree_id: T, keys: Vec<K>) -> SMTResult<Vec<u8>>;
 
     /// Calculate the future root hash before updating data
     fn get_next_root(&self, old_proof: Vec<u8>, next_kvs: Vec<(K, V)>) -> Result<H256, Error>;
 
     /// Delete/clear a specific Merkle tree
-    fn clear(&self, tree_id: impl AsRef<[u8]>) -> Result<(), Error>;
+    fn clear(&self, tree_id: T) -> Result<(), Error>;
 
     /// Verify a Merkle proof
     fn verify(&self, proof: Proof<K, V>) -> bool;
